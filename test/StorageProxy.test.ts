a0cfedfefbce8a51cb08c9f3b44d0187c055d01e
slikts <dabas@untu.ms>
2018-07-10 23:55:34 +0300

    Init

---
+++ test/StorageProxy.test.ts
@@ -0,0 +1,7 @@
+import StorageProxy from '../src/StorageProxy'
+
+describe('', () => {
+  it('StorageProxy is instantiable', () => {
+    expect(new StorageProxy()).toBeInstanceOf(StorageProxy)
+  })
+})
